7518de556c792dbbc60010498867319296bc7056
Chakphet Wongmanee <king_23619@hotmail.com>
2024-07-04 22:56:36 +0700

    chore: change db config and add dockerfile

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,11 +1,7 @@
 require('dotenv').config()
 
 const dbConfig = {
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT,
+    uri:process.env.DATABASE_URL,
     waitForConnections: true,
 }
 
