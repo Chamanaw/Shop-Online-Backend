8a4b874000c5a015d0aaf1e1965780032316908b
Chakphet Wongmanee <king_23619@hotmail.com>
2024-07-05 17:04:35 +0700

    fix: response error

--- routes/ping.routes.js
+++ routes/ping.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/pingserver',(res)=>{res.send()})
+router.get('/pingserver',(req,res)=>{res.sendStatus(200)})
 
 module.exports = router
